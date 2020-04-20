f08cd7791058c9cd526fa3d98b83dbe094bd4f28
Megan Comeau <mlcomeau99@gmail.com>
2020-04-20 19:15:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
       if !(genre_count.include?(genre))
         genre_count[genre] = 1
       else
-        genre_count[genre] += 1 
+        genre_count[genre] += 1
       end
     end
 
