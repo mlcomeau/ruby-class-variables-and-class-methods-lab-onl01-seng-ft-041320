4541f4c5c832443e9aa95d501bfd69b7b53a05f0
Megan Comeau <mlcomeau99@gmail.com>
2020-04-20 19:17:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,24 @@ class Song
     end
 
     genre_count
+    
   end
+
+  def self.artist_count
+    artist_count ={}
+
+    @@artists.each do |artist|
+      if !(artist_count.include?(artist))
+        artist_count[artist] = 1
+      else 
+        artist_count[artist] +=1
+      end
+    end
+
+    artist_count
+
+  end
+  
 
 
 
